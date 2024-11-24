90a55ee40dc9b171b4a447d7c696b228a7fd3c2a
Keith P. Jolley <keith.jolley@zainartech.com>
2024-11-24 07:09:26 -0800

    chore: save

--- scripts/startjupyter.sh
+++ scripts/startjupyter.sh
@@ -4,7 +4,6 @@
 # squalor heights, ca, usa
 # Fri Dec 21 06:53:31 PST 2018
 #
-set -e
 deactivate 2>/dev/null || true # Fails if not in a virtualenv, that's OK.
 . /tmp/venv/bin/activate
-jupyter notebook # --ip=127.0.0.1 # --port=8888
+jupyter notebook # --ip=127.0.0.1 --port=8888
